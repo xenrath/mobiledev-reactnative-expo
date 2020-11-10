e235a021ef2d2010e3c599f7e2ce0bbf0424e4b0
xenrath <xenrath89@gmail.com>
2020-11-10 10:40:52 +0700

    Membuat Aplikasi Android / iOS dengan React Native + Expo

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  click(){
+    Alert.alert('Hei, I Miss U')
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Pesan dari X E N R 4 T H</Text>
+        <Button onPress={ this.click } title="Click Me !" color="#009688" />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
